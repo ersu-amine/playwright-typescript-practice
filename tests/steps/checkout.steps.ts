730ad2d3d59805b3633fd85ab0b0a4ff6423eba6
ersu-amine <ersu.amine@gmail.com>
2025-10-30 01:32:07 -0400

    remove incorrect equal sign from snippets

--- tests/steps/checkout.steps.ts
+++ tests/steps/checkout.steps.ts
@@ -1,39 +1,23 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 
-When("user clicks shopping cart icon", async function () {
+When("user clicks shopping cart icon", async function () {});
 
-});
+When("user click proceed to checkout button", async function () {});
 
-When("user click proceed to checkout button", async function () {
-
-});
-
-When("user logs in", async function () {
-
-});
+When("user logs in", async function () {});
 
 When(
   "user fills in the shipping address details and click proceeds to checkout button",
-  async function () {
-
-  }
+  async function () {}
 );
 
-When("user select payment method <{string}>", async function (string) {
-
-});
+When("user select payment method <{string}>", async function (string) {});
 
 When(
   "user enters valid payment details for <{string}>",
-  async function (string) {
-=
-  }
+  async function (string) {}
 );
 
-When("user clicks confirm button", async function () {
+When("user clicks confirm button", async function () {});
 
-});
-
-Then("user should see an order confirmation message", async function () {
-
-});
+Then("user should see an order confirmation message", async function () {});
